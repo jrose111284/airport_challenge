bdb9ca72bef0d3699140ea274d4b23109fe8baa0
jamierose <jrose111284@gmail.com>
2015-11-02 19:40:36 +0000

    user story 2

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -2,6 +2,12 @@ describe Airport do
   it 'so planes can land at a airport' do
     airport = Airport.new
     plane = Plane.new
-    #
+    expect{ airport.land(plane) }.not_to raise_error
+  end
+
+  it 'so planes can take off from airport' do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.take_off(plane) }.not_to raise_error
   end
 end
