0c0cfb82d259341c2258a8a9da7d59a9e7af2502
jamierose <jrose111284@gmail.com>
2015-11-03 09:16:16 +0000

    user story 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,13 @@
 class Airport
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
+
+  end
 
   def land(plane)
+    raise 'cannot land plane, at full capacity' if @planes.length >= @capacity
+    @planes << plane
   end
 
   def take_off(plane)
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,13 +1,22 @@
 describe Airport do
   it 'so planes can land at a airport' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect{ airport.land(plane) }.not_to raise_error
   end
 
   it 'so planes can take off from airport' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.take_off(plane) }.not_to raise_error
+  end
+
+  it 'does not allow airplanes to land when airport is full' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    20.times do
+      airport.land(20)
+    end
+    expect { airport.land(plane) }.to raise_error 'cannot land plane, at full capacity'
   end
 end
