e7752dfbb0f730b20db8598d556b066531aa574b
jamierose <jrose111284@gmail.com>
2015-11-16 12:10:48 +0000

    airport challenge complete

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,45 @@
 class Airport
-  def initialize(capacity)
-    @capacity = capacity
-    @planes = []
+  DEFAULT_CAPACITY = 20
+  attr_reader :planes
 
+  def initialize(opts)
+    @capacity = opts.fetch(:capacity, DEFAULT_CAPACITY)
+    @weather_reporter = opts[:weather_reporter]
+    @planes = []
   end
 
   def land(plane)
-    raise 'cannot land plane, at full capacity' if @planes.length >= @capacity
-    @planes << plane
+    raise 'Cannot land plane: airport full' if full?
+    raise 'Cannot land plane: weather is stormy' if stormy?
+    plane.land(self)
+    planes << plane
   end
 
   def take_off(plane)
+    raise 'Cannot take off plane: weather is stormy' if stormy?
+    raise 'Cannot take off plane: plane not at this airport' unless at_airport?(plane)
+    plane.take_off
+    remove_plane(plane)
+    plane
   end
 
+  private
+
+  attr_reader :capacity, :weather_reporter
+
+  def full?
+    planes.length >= capacity
+  end
+
+  def stormy?
+    weather_reporter.stormy?
+  end
+
+  def at_airport?(plane)
+    planes.include? plane
+  end
+
+  def remove_plane(plane)
+    planes.delete_if { | landed_plane | landed_plane == plane }
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,20 +1,102 @@
 require 'airport'
 
 describe Airport do
-  subject(:airport) { described_class.new(20) }
-    it 'instructs a plane to land' do
-    expect(airport).to respond_to(:land).with(1).argument
-   end
+  let(:weather_reporter) { double :weather_reporter }
+  subject(:airport) { described_class.new(capacity: 20, weather_reporter: weather_reporter) }
+  let(:plane) { double :plane, land: nil, take_off: nil }
 
-   it 'instructs a plane to take off' do
-     expect(airport).to respond_to(:take_off).with(1).argument
-   end
+  describe '#land' do
+    context 'when not stormy' do
+      before do
+        allow(weather_reporter).to receive(:stormy?).and_return false
+      end
 
-   it 'does not allow landing when at capacity' do
-     plane = double :plane
-     20.times do
-       airport.land(plane)
-     end
-     expect{ airport.land(plane) }.to raise_error 'cannot land plane, at full capacity'
-   end
+      it 'instructs a plane to land' do
+        expect(plane).to receive(:land)
+        airport.land(plane)
+      end
+
+      context 'when full' do
+        before do
+          20.times { airport.land(plane) }
+        end
+
+        it 'raises an error' do
+          expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
+        end
+      end
+    end
+
+    context 'when stormy' do
+      before do
+        allow(weather_reporter).to receive(:stormy?).and_return true
+      end
+
+      it 'raises an error' do
+        expect { airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
+      end
+    end
+  end
+
+  describe '#take_off' do
+    context 'when not stormy' do
+      before do
+        allow(weather_reporter).to receive(:stormy?).and_return false
+      end
+
+      it 'instructs a plane to take off' do
+        airport.land(plane)
+        expect(plane).to receive(:take_off)
+        airport.take_off(plane)
+      end
+
+      it 'raises an error if plane is not at this airport' do
+        other_airport = described_class.new(capacity: 20, weather_reporter: weather_reporter)
+        other_airport.land(plane)
+        expect { airport.take_off(plane) }.to raise_error 'Cannot take off plane: plane not at this airport'
+      end
+
+      it 'returns the plane that took off' do
+        airport.land(plane)
+        expect(airport.take_off(plane)).to eq plane
+      end
+    end
+
+    context 'when stormy' do
+      before do
+        allow(weather_reporter).to receive(:stormy?).and_return true
+      end
+
+      it 'raises an error' do
+        expect { airport.take_off(plane) }.to raise_error 'Cannot take off plane: weather is stormy'
+      end
+    end
+  end
+
+  describe '#planes' do
+    before do
+      allow(weather_reporter).to receive(:stormy?).and_return false
+    end
+
+    it 'returns planes at the airport' do
+      airport.land(plane)
+      expect(airport.planes).to include plane
+    end
+
+    it 'does not return planes that have taken off' do
+      airport.land(plane)
+      airport.take_off(plane)
+      expect(airport.planes).not_to include plane
+    end
+  end
+
+  context 'defaults' do
+    subject(:default_airport) { Airport.new(weather_reporter: weather_reporter) }
+
+    it 'has a default capacity' do
+      allow(weather_reporter).to receive(:stormy?).and_return false
+      Airport::DEFAULT_CAPACITY.times { default_airport.land(plane) }
+      expect { default_airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
+    end
+  end
 end
